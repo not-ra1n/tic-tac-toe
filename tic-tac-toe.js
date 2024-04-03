3d10cbe00ef3203a18e3e5321dcf16ca4e06e7a5
not-ra1n <142693849+not-ra1n@users.noreply.github.com>
2024-04-03 04:34:14 -0400

    Implement restart button functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -64,6 +64,14 @@ function gameController (
     const getWins = () => win;
     const getTies = () => tie;
 
+    function resetWins () {
+        return win = 0;
+    }
+
+    function resetTies () {
+        return tie = 0;
+    }
+
 // while (win === 0 || tie === 0) {
 // }
 
@@ -211,29 +219,32 @@ function winTieLogic () {
 }
 
     
-    return {getActivePlayer, switchPlayerTurn, winTieLogic, getWins, getTies};
+    return {getActivePlayer, switchPlayerTurn, winTieLogic, getWins, getTies
+    , resetWins, resetTies};
 
 }
 
 
 function displayController () {
     
-    let box1 = document.querySelector("#box-1")
-    let box2 = document.querySelector("#box-2")
-    let box3 = document.querySelector("#box-3")
-    let box4 = document.querySelector("#box-4")
-    let box5 = document.querySelector("#box-5")
-    let box6 = document.querySelector("#box-6")
-    let box7 = document.querySelector("#box-7")
-    let box8 = document.querySelector("#box-8")
-    let box9 = document.querySelector("#box-9")
+    let box1 = document.querySelector("#box-1");
+    let box2 = document.querySelector("#box-2");
+    let box3 = document.querySelector("#box-3");
+    let box4 = document.querySelector("#box-4");
+    let box5 = document.querySelector("#box-5");
+    let box6 = document.querySelector("#box-6");
+    let box7 = document.querySelector("#box-7");
+    let box8 = document.querySelector("#box-8");
+    let box9 = document.querySelector("#box-9");
 
-    let startButton = document.querySelector("#start-button")
-    let playerDisplay = document.querySelector("#playerDisplay")
+    let startButton = document.querySelector("#start-button");
+    let restartButton = document.querySelector("#restart-button");
+    let playerDisplay = document.querySelector("#playerDisplay");
 
     const board = gameBoard();
 
-    const {getActivePlayer, switchPlayerTurn, winTieLogic, getWins, getTies} = gameController();
+    const {getActivePlayer, switchPlayerTurn, winTieLogic, getWins, getTies
+    , resetWins, resetTies} = gameController();
 
     console.log(getWins())
 
@@ -267,10 +278,47 @@ function displayController () {
         startButtonClicked = 1;
     });
 
-    function playerDisplayFunction () {
-        playerDisplay.textContent = (`${getActivePlayer().name}'s turn`)
-    }
+    restartButton.addEventListener("click", function(){
+        rowOne[0] = "";
+        rowOne[1] = "";
+        rowOne[2] = "";
+        rowTwo[0] = "";
+        rowTwo[1] ="";
+        rowTwo[2] = "";
+        rowThree[0] = "";
+        rowThree[1] = "";
+        rowThree[2] = "";
+        box1.textContent = rowOne[0];
+        box2.textContent = rowOne[1];
+        box3.textContent = rowOne[2]; 
+        box4.textContent = rowTwo[0]; 
+        box5.textContent = rowTwo[1]; 
+        box6.textContent = rowTwo[2]; 
+        box7.textContent = rowThree[0]; 
+        box8.textContent = rowThree[1]; 
+        box9.textContent = rowThree[2]; 
 
+        resetWins();
+        resetTies();
+
+        startButtonClicked = 1;
+
+        if (getActivePlayer().marker === "O") {
+            switchPlayerTurn();
+            playerDisplayFunction()
+        } else {
+            playerDisplayFunction()
+        }
+
+    });
+
+    function playerDisplayFunction () {
+        if (getWins() !== 0 || getTies() !== 0) {
+            playerDisplay.textContent = (`${getActivePlayer().name} won!`)
+        } else {
+            playerDisplay.textContent = (`${getActivePlayer().name}'s turn`)
+        }
+    }
     box1.addEventListener("click", function() {
         if (box1.textContent === "" && getWins() === 0 && getTies() === 0
         && startButtonClicked !== 0) {
@@ -278,9 +326,17 @@ function displayController () {
         rowOne[0] = box1.textContent
         winTieLogic();
         console.log(getWins())
+
     
-        switchPlayerTurn();
-        playerDisplayFunction();
+
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
+
+
         console.log(switchPlayerTurn)
     }
         
@@ -293,8 +349,13 @@ function displayController () {
         rowOne[1] = box2.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -307,8 +368,13 @@ function displayController () {
         rowOne[2] = box3.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -321,8 +387,13 @@ function displayController () {
         rowTwo[0] = box4.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -335,8 +406,13 @@ function displayController () {
         rowTwo[1] = box5.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -352,8 +428,13 @@ function displayController () {
         console.log(rowTwo[1])
         console.log(rowTwo[2])
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -366,8 +447,13 @@ function displayController () {
         rowThree[0] = box7.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -380,8 +466,13 @@ function displayController () {
         rowThree[1] = box8.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -394,8 +485,13 @@ function displayController () {
         rowThree[2] = box9.textContent
         winTieLogic();
         console.log(getWins())
-        switchPlayerTurn();
-        playerDisplayFunction();
+        
+        if (getWins() === 0 && getTies() === 0) {
+            switchPlayerTurn();
+            playerDisplayFunction();
+        } else {
+            playerDisplayFunction();
+        }
 
     }
         
@@ -407,5 +503,3 @@ function displayController () {
 displayController();
 
 gameController();
-
-
